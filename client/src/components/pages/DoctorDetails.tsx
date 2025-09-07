470fa6602659fbcbf57f5ee7d412d3a191e45775
ahmed nabil <am4201828@gmail.com>
2025-09-07 14:55:01 +0300

    doctorpage

--- client/src/components/pages/DoctorDetails.tsx
+++ client/src/components/pages/DoctorDetails.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { doctors } from "../json/doctors.json";
+import {
+  doctor_appointments as appointment,
+  times,
+} from "../json/doctortimes.json";
 
 type doctorData = {
   id: number;
@@ -8,19 +12,27 @@ type doctorData = {
   specialization: string;
   status: string;
   image: string;
-  about:string;
-  degree:string;
-  fee:string;
-  experience:string;
-}
+  about: string;
+  degree: string;
+  fee: string;
+  experience: string;
+};
 
 function DoctorDetails() {
   const { id } = useParams();
-  const [doctor, setDoctor] = useState<doctorData | null >(null);
+  const [doctor, setDoctor] = useState<doctorData | null>(null);
   useEffect(() => {
-    const doc : doctorData = doctors.filter((doc) => doc.id === Number(id))[0];
+    const doc: doctorData = doctors.filter((doc) => doc.id === Number(id))[0];
     setDoctor(doc);
   }, []);
+  
+  const btnActivehandler = (btn, className) => {
+    document
+      .querySelectorAll(className)
+      .forEach((e) => e.classList.remove("active"));
+    btn.classList.add("active");
+  };
+
   return (
     <section className="specia_doctor_section">
       <div className="doctor_info">
@@ -29,11 +41,43 @@ function DoctorDetails() {
         </div>
         <div className="doctor_data">
           <h1>{doctor?.name}</h1>
-          <h4>{doctor?.degree} - {doctor?.specialization} <span>{doctor?.experience}</span></h4>
+          <h4>
+            {doctor?.degree} - {doctor?.specialization}{" "}
+            <span>{doctor?.experience}</span>
+          </h4>
           <p className="about">About</p>
           <p className="about-content">{doctor?.about}</p>
-          <p className="doctor-fee">Appointment fee: <span>{doctor?.fee}</span></p>
+          <p className="doctor-fee">
+            Appointment fee: <span>{doctor?.fee}</span>
+          </p>
         </div>
+      </div>
+      <div className="Booking_Slot">
+        <h2>Booking slots</h2>
+        <div className="doctor_dates">
+          {appointment.map((item, idx) => (
+            <button
+              className="doctor_date"
+              key={idx}
+              onClick={(e) => btnActivehandler(e.currentTarget, ".doctor_date")}
+            >
+              <h6>{item.day}</h6>
+              <p>{item.dayDate}</p>
+            </button>
+          ))}
+        </div>
+        <div className="doctor_times">
+          {times.map((item, idx) => (
+            <button
+              className="doctor_time"
+              key={idx}
+              onClick={(e) => btnActivehandler(e.currentTarget, ".doctor_time")}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+        <button className="appointmentBtn">Book an appointment</button>
       </div>
     </section>
   );
