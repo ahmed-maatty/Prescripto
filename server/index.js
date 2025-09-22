585760981120247ad7184a0998717c31d9354939
ahmed nabil <am4201828@gmail.com>
2025-09-22 22:33:01 +0300

    server is ready

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 import Express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 const app = Express();
+app.use(Express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(cookieParser());
 
-const Port = process.env.PORT
-app.listen(Port , () => {
-  console.log(`Server Work On Port ${Port}`)
-})
+const Port = process.env.PORT;
+app.listen(Port, () => {
+  console.log(`Server Work On Port ${Port}`);
+});
